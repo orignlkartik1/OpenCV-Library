01a9241d2ef50779180ac16c83882f4a11708be5
Orignlkartik1 <kartik964364@gmail.com>
2025-08-26 20:57:36 +0530

    Initial commit

---
+++ image_first.py
@@ -0,0 +1,22 @@
+# Import Libraries or Setup environment
+import cv2 as cv
+import sys
+
+# read an image file
+img=cv.imread("you_img_file",cv.IMREAD_GRAYSCALE) # In the second argument, I'm changing the color of image. By default, It is IMAGE_COLOR BGR (8-BIT format). You can use other formats.
+
+# by using sys module if output is none, it stops the program
+if img is None:
+    sys.exit("Image is not Available.")
+
+# Display a window of your image
+cv.imshow("Display Window",img)
+
+# This use for img waits until any key is not press.
+k=cv.waitKey(0)
+
+# if key pressed is small case letter of 's,' then it saves your image in cwd.
+if k==ord("s"):
+
+    # this use for saving file in your current working directory.
+    cv.imwrite("choose_file.png",img)
